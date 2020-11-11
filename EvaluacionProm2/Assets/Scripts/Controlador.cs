b7befb11f8c4f8dcf4e376fcfe54479faf971643
JoseCabrera9608 <73959334+JoseCabrera9608@users.noreply.github.com>
2020-11-11 17:59:49 -0500

    Controlador JoseEduardo
    Controlador

---
+++ EvaluacionProm2/Assets/Scripts/Controlador.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controlador : MonoBehaviour
+{
+    private Modelo modelo;
+    GameObject temporalPersonajeRojo;
+    GameObject TemporalPersonajeVerde;
+    GameObject temporalPersonajeAzul;
+    void Start()
+    {
+        modelo = GetComponent<Modelo>();
+    }
+
+    public void InstanciarPersonajeRojo()
+    {
+        temporalPersonajeRojo = Instantiate(modelo.personajes[0]);
+        temporalPersonajeRojo.transform.position = modelo.spawnPoint.transform.position;
+
+        
+    }
+    public void InstanciarPersonajeVerde()
+    {
+        TemporalPersonajeVerde = Instantiate(modelo.personajes[1]);
+        TemporalPersonajeVerde.transform.position = modelo.spawnPoint.transform.position;
+
+    }
+    public void InstanciarPersonajeAzul()
+    {
+        temporalPersonajeAzul = Instantiate(modelo.personajes[2]);
+        temporalPersonajeAzul.transform.position = modelo.spawnPoint.transform.position
+
+    }
+    public void Seleccion()
+    {
+        switch (modelo.personajesBoton)
+        {
+            case 0:
+                InstanciarPersonajeRojo();
+                break;
+            
+                
+        }
+    }
+}
---
+++ EvaluacionProm2/Assets/Scripts/Modelo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Modelo : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    public GameObject[] personajesBoton;
+    public GameObject[] personajes;
+    
+    void Start()
+    {
+        
+    }
+
+  
+}
